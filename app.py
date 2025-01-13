47146d1a98f93a864aa991e31d7b34c051336e3a
mmjafet <l202123653@jilotepec.tecnm.mx>
2025-01-13 01:51:56 -0600

    new

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def predict():
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8500)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
